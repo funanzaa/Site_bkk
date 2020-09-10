002bcf219e4082ef15cb7a63215f9efed8ed84cb
funanzaa <eakachai_golf@hotmail.com>
2020-09-10 12:35:45 +0700

    authen dashborad

--- SiteCrm/crm/urls.py
+++ SiteCrm/crm/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     path('dashboard', views.dashboardPage, name='dashboard-page'),
-    # path('create_case/', views.createCase, name='create_case'),
+    path('create_case/', views.createCase, name='create_case'),
     # path('update_case/<str:pk>', views.updateCase, name='update_case'),
     # path('delete_case/<str:pk>', views.deleteCase, name='delete_case'),
-    # path('hospital/', views.hospital, name='hospital-page'),
-    # path('create_hospital/', views.hospitalAdd, name='create_hospital'),
+    path('hospital/', views.hospital, name='hospital-page'),
+    path('create_hospital/', views.hospitalAdd, name='create_hospital'),
     # path('create_case_hospital/<str:pk>', views.create_case_hospital, name='create_case_hospital'),
     # path('edit_hospital/<str:pk>', views.hospitalEdit, name='edit_hospital'),
 ]
